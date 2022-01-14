808e87dfd63ae90fab74de6d8dc08066722b74b8
Martin Tudor <martcan27@gmail.com>
2022-01-14 13:20:57 +0000

    update

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: [
-      'res.cloudinary.com'
-    ],
+    domains: ['res.cloudinary.com', 't.scdn.co'],
   },
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,95 +1,74 @@
-import Head from 'next/head';
+import { getSecrets } from '@netlify/functions'
+import Head from 'next/head'
 
-import Layout from '@components/Layout';
-import Container from '@components/Container';
+import Layout from '@components/Layout'
+import Container from '@components/Container'
 
 import styles from '@styles/Home.module.scss'
+import Image from 'next/image'
 
-export default function Home() {
+export default function Home({ categories }) {
+  console.log(categories)
   return (
     <Layout>
       <Head>
         <title>Spotify Re-Wrapped</title>
-        <meta name="description" content="Top artists and tracks for the last whatever period of time..." />
+        <meta
+          name="description"
+          content="Top artists and tracks for the last whatever period of time..."
+        />
       </Head>
 
       <Container>
         <h1 className="sr-only">Spotify Re-Wrapped</h1>
 
-        <h2 className={styles.heading}>Top Artists</h2>
+        <h2 className={styles.heading}>Categories</h2>
 
         <ul className={styles.items}>
-          <li>
-            <a href="https://www.blink182.com/">
-              <img width="280" src="/images/blink-182.jpg" alt="Artist Photo" />
-              <p className={styles.itemTitle}>
-                blink-182
-              </p>
-            </a>
-          </li>
-          <li>
-            <a href="https://www.blink182.com/">
-              <img width="280" src="/images/blink-182.jpg" alt="Artist Photo" />
-              <p className={styles.itemTitle}>
-                blink-182
-              </p>
-            </a>
-          </li>
-          <li>
-            <a href="https://www.blink182.com/">
-              <img width="280" src="/images/blink-182.jpg" alt="Artist Photo" />
-              <p className={styles.itemTitle}>
-                blink-182
-              </p>
-            </a>
-          </li>
-          <li>
-            <a href="https://www.blink182.com/">
-              <img width="280" src="/images/blink-182.jpg" alt="Artist Photo" />
-              <p className={styles.itemTitle}>
-                blink-182
-              </p>
-            </a>
-          </li>
-        </ul>
+          {categories.items.map((item) => {
+            return (
+              <li key={item.id}>
+                <a href="">
+                  <div
+                    style={{
+                      position: 'relative',
+                      width: '150px',
+                      height: '150px',
+                    }}
+                  >
+                    <Image
+                      src={item.icons[0].url}
+                      alt="Artist Photo"
+                      layout="fill"
+                      ObjectFit="cover"
+                    />
+                  </div>
 
-        <h2 className={styles.heading}>Top Tracks</h2>
-
-        <ul className={styles.items}>
-          <li>
-            <a href="https://www.blink182.com/">
-              <img width="280" src="/images/dude-ranch.jpg" alt="Dude Ranch Album Cover" />
-              <p className={styles.itemTitle}>
-                Enthused
-              </p>
-            </a>
-          </li>
-          <li>
-            <a href="https://www.blink182.com/">
-              <img width="280" src="/images/dude-ranch.jpg" alt="Dude Ranch Album Cover" />
-              <p className={styles.itemTitle}>
-                Enthused
-              </p>
-            </a>
-          </li>
-          <li>
-            <a href="https://www.blink182.com/">
-              <img width="280" src="/images/dude-ranch.jpg" alt="Dude Ranch Album Cover" />
-              <p className={styles.itemTitle}>
-                Enthused
-              </p>
-            </a>
-          </li>
-          <li>
-            <a href="https://www.blink182.com/">
-              <img width="280" src="/images/dude-ranch.jpg" alt="Dude Ranch Album Cover" />
-              <p className={styles.itemTitle}>
-                Enthused
-              </p>
-            </a>
-          </li>
+                  <p className={styles.itemTitle}>{item.name}</p>
+                </a>
+              </li>
+            )
+          })}
         </ul>
       </Container>
     </Layout>
   )
+}
+
+export async function getServerSideProps() {
+  const secrets = await getSecrets()
+
+  const categoriesResponce = await fetch(
+    'https://api.spotify.com/v1/browse/categories?limit=10&locale=en_gb',
+    {
+      headers: { Authorization: `Bearer ${secrets.spotify.bearerToken}` },
+    }
+  )
+  const data = await categoriesResponce.json()
+
+  return {
+    props: {
+      categories: data.categories,
+    },
+  }
 }
